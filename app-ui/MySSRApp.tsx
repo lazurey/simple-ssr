979d51132d81fcba022ba31abd3c19b8a7ab4f4e
Sause <lazureys@gmail.com>
2018-07-30 17:47:37 +0800

    2.1 add a simply component

--- app-ui/MySSRApp.tsx
+++ app-ui/MySSRApp.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
 
+import { SayHi } from './pages/SayHi';
+
 export class MySSRApp extends React.Component<any, any> {
   render() {
-    return (<h1>Hello SSR</h1>);
+    return (<main>
+      <h1>Hello SSR</h1>
+      <SayHi />
+    </main>);
   }
 };
---
+++ app-ui/pages/SayHi.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+
+const titleStyle = {
+  fontSize: '30px',
+}
+
+export class SayHi extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      showMsg: false,
+    };
+  }
+
+  say = () => {
+    this.setState({
+      showMsg: true,
+    });
+    setTimeout(() => {
+      this.setState({
+        showMsg: false,
+      });
+    }, 2000);
+  }
+  
+  render() {
+    return (
+      <div>
+        <button onClick={this.say}>Say Hi!</button>
+        { this.state.showMsg && (<div style={titleStyle}>Hi</div>) }
+      </div>
+    )
+  }
+};
